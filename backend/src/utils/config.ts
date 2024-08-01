b84d9f00cf0eb98d5358dfdef3ad4e76d1fd096a
Hitendra Panchal <92884470+HitendrakPanchal@users.noreply.github.com>
2024-08-01 16:01:46 +0530

    Update and rename config.ts to example.config.ts

--- backend/src/utils/config.ts
+++
@@ -1,3 +0,0 @@
-// export const mongoURI = 'mongodb+srv://mngdb-chessmate:Mongo-cluster-19-ChessMate@cluster-chessmate.o2q3kx1.mongodb.net/';
-export const mongoURI = 'mongodb+srv://mngdb-chessmate:Mongo-cluster-19-ChessMate@cluster-chessmate.o2q3kx1.mongodb.net/mydb';
-export const jwtSecret = '12345';
---
+++ backend/src/utils/example.config.ts
@@ -0,0 +1,2 @@
+export const mongoURI = 'YOUR_MONGODB_CLUSTER_URL';
+export const jwtSecret = 'YOUR_JWTSECRET_KEY';
